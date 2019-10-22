cc7716087f45eec989e26fd0cb9b04805f57b1f7
Ethan Miller <ethan.romans5.8@gmail.com>
2019-10-21 21:09:03 -0500

    replace hard coded list with calls to the db in service

--- src/main/java/org/hibernate/jht/TopicRepository.java
+++ src/main/java/org/hibernate/jht/TopicRepository.java
@@ -3,6 +3,7 @@ package org.hibernate.jht;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TopicRepository extends CrudRepository<Topic, String> {
+    Topic findOne(String id);
 //    getAllTopics();
 //    getTopic(String id);
 //    updateTopic(Topic t);
--- src/main/java/org/hibernate/jht/TopicService.java
+++ src/main/java/org/hibernate/jht/TopicService.java
@@ -13,39 +13,26 @@ public class TopicService {
     @Autowired
     private TopicRepository topicRepository;
 
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-            new Topic("spring", "Spring Framework", "Spring Framework Description"),
-            new Topic("java", "Core Java", "Core Java Description"),
-            new Topic("javascript", "JavaScript", "JavaScript Description")
-    ));
-
     public List<Topic> getAllTopics() {
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll()
+                .forEach(topics::add);
         return topics;
     }
 
     public Topic getTopic(String id) {
-        return topics.stream()
-                .filter(t -> t.getId().equalsIgnoreCase(id))
-                .findFirst()
-                .get();
+        return topicRepository.findOne(id);
     }
 
     public void addTopics(Topic topic) {
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
-    public void updateTopic(String id, Topic topic) {
-        for (int i = 0; i < topics.size(); i++) {
-            Topic t = topics.get(i);
-            if (t.getId().equalsIgnoreCase(id)) {
-                topics.set(i, topic);
-                return;
-            }
-
-        }
+    public void updateTopic(Topic topic) {
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id) {
-        topics.removeIf(t -> t.getId().equalsIgnoreCase(id));
+        topicRepository.delete(id);
     }
 }
